85792dae2ac34c2d057d1208dddcb122da1a1233
Tony Le <letony@icloud.com>
2019-05-07 15:59:10 +1000

    Basic jump implemented; (Will probably convert to a rigidbody based controller. Allow for bumping etc)

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using pokoro;
+using UnityEngine.Events;
 
 namespace SoulFood
 {
@@ -9,38 +10,50 @@ namespace SoulFood
     public class PlayerMoveController : MonoBehaviour
     {
         [Header("Move")]
-        [SerializeField] float maxSpeed = 5f;
+        [SerializeField] float maxSpeed = 10f;
 
 
         [Header("Dash")]
-        [SerializeField] float dashSpeed = 20f;
-        [SerializeField] float dashReduction = 1f;
+        [SerializeField] float dashSpeed = 13f;
+        [SerializeField] float dashReduction = 1.5f;
         private bool isDashing;
         float currentDashAmount;
 
 
         [Header("Jump")]
-        [SerializeField] float jumpForce;
+        [SerializeField] float jumpForce = 23.5f;
+        [SerializeField] float gravity = 3.5f;
         private bool isJumping;
-        private float gravity = 9.81f;
-        float currentJumpAmount;
+        private Vector3 jumpMotion;
+
+
+        // [Header("Events")]
+        // UnityEvent OnJump;
+        // OnLanded, OnDash, OnPickup, OnAction;
+
 
         [Header("IMGUI style")]
         [SerializeField] bool displayDebug = true;
-        [SerializeField] GUIStyle debug;
+        [SerializeField] GUIStyle style;
 
 
         //CACHES
         new Camera camera;      //Required so that movement aligns with the view
         PlayerInput input;
-        CharacterController controller;
+        CharacterController charctrl;
 
+
+
+        void Awake()
+        {
+            camera = Camera.main;
+            charctrl = GetComponent<CharacterController>();
+            input = GetComponent<PlayerInput>();
+        }
 
         void Start()
         {
-            camera = Camera.main;
-            controller = GetComponent<CharacterController>();
-            input = GetComponent<PlayerInput>();
+            // OnJump.AddListener()
         }
 
         void Update()
@@ -53,16 +66,16 @@ namespace SoulFood
             if (!displayDebug)
                 return;
                 
-            GUILayout.Label("Controls: WASD Move. J Pick Up. K Action. L Dash. Space Jump", debug);
-            if (input.isPickup) GUILayout.Label("Picking up!", debug);
-			if (input.isAction) GUILayout.Label("Action!", debug);
-			if (input.isDash) GUILayout.Label("Dashing!", debug);
-            if (input.isJump) GUILayout.Label("Jumping!", debug);
+            GUILayout.Label("Controls: WASD Move. J Pick Up. K Action. L Dash. Space Jump", style);
+            if (input.isPickup) GUILayout.Label("Picking up!", style);
+			if (input.isAction) GUILayout.Label("Action!", style);
+			if (input.isDash) GUILayout.Label("Dashing!", style);
+            if (input.isJump) GUILayout.Label("Jumping!", style);
 
-			if (input.wasPickup) GUILayout.Label("Picked up!", debug);
-            if (input.wasAction) GUILayout.Label("Actioned!", debug);
-            if (input.wasDash) GUILayout.Label("Dashed!", debug);
-            if (input.wasJump) GUILayout.Label("Jumped!", debug);
+			if (input.wasPickup) GUILayout.Label("Picked up!", style);
+            if (input.wasAction) GUILayout.Label("Actioned!", style);
+            if (input.wasDash) GUILayout.Label("Dashed!", style);
+            if (input.wasJump) GUILayout.Label("Jumped!", style);
         }
 
         void MoveCharacter()
@@ -70,7 +83,8 @@ namespace SoulFood
             //Set basic speed
             var speedTotal = maxSpeed;
 
-			//// Dash
+#region Dash
+            //Calculate dash boost
             if (isDashing)   //Start reducing the dash boost (linearly) if currently dashing
             {
                 currentDashAmount -= dashReduction;
@@ -89,18 +103,35 @@ namespace SoulFood
             }
             //Add dash
             speedTotal += currentDashAmount;
+#endregion
+
+#region Jump
+            //Calculate jump motion
+            // Vector3 jumpMotion = Vector3.zero;
+
+            if (charctrl.isGrounded)
+            {
+                jumpMotion.y = -gravity;
+                if (input.wasJump)
+                {
+                    // OnJump.Invoke();
+                    jumpMotion.y = jumpForce;
+                }
+            }
+            else 
+            {
+                jumpMotion.y -= gravity;
+            }
+#endregion
 
             //Calculate move motion vector
             Vector3 moveMotion = input.xAxis * camera.transform.RightSansYNormalized() * speedTotal + 
                                 input.yAxis * camera.transform.ForwardSansYNormalized() * speedTotal;
 
-            //Calculate jump motion vector
-            Vector3 jumpMotion = Vector3.zero;  //TODO
-
             //Combine all motion vectors
             Vector3 finalMotion = moveMotion + jumpMotion;
 
-            controller.Move(finalMotion * Time.deltaTime);
+            charctrl.Move(finalMotion * Time.deltaTime);
         }
     }
 }
